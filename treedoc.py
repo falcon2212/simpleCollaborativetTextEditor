1580c052a3ce3c3004119d946899bf1c4d349e9c
falcon2212 <khalid.mrkool@gmail.com>
2020-05-24 21:39:27 +0530

    implementation with better efficiency

--- treedoc.py
+++ treedoc.py
@@ -1,52 +1,60 @@
 from math import *
-from copy import copy
+from copy import *
 def dfs(node):
 	szleft = 0
 	szright = 0
-	for i in node.adjacencyListLeft:
-		szleft+=dfs(node.adjacencyListLeft[i])	
-	for i in node.adjacencyListRight:
-		szright+=dfs(node.adjacencyListRight[i])	
+	if(node == None):
+		return 0
+	szleft+=dfs(node.left)	
+	szright+=dfs(node.right)	
 	node.size = szright+szleft
 	if(node.value != ""):
 		node.size+=1
 	return node.size	
+#returns true if node1 is ancestor of node2		
+def ancestor(node1, node2):
+	par = node2;
+	while par != None:
+		if(par == node1):
+			return True
+		par = par.parent	
+	return False	
 class Node:
 	def __init__(self, data):
 		self.value = data
 		#adjacency list stores the information of children as {(bit, disambiguator): childNode}
-		self.adjacencyListLeft = dict()
-		self.adjacencyListRight = dict()
+		self.left = None
+		self.right = None
 		self.counter = 0
 		self.size = 1
+		self.parent = None
 		if(data == ""):
 			self.size = 0
 
 	#returns node corresponding to the character at pos in crdt	
 	def query(self, n):
-		dfs(self)
+		if(self.size == 0):
+			return self
 		# print "Query ", n, self.size
 		if(n > self.size):
 			return None
 		def util(node, k, pos, ans):
-			if(len(node.adjacencyListLeft) > 0):
-				for i in node.adjacencyListLeft:
-					j = node.adjacencyListLeft[i]
-					util(j, k, pos, ans)
-					if(len(ans) > 0):
-						return
+			if(node.left != None):
+				j = node.left
+				util(j, k, pos, ans)
+				if(len(ans) > 0):
+					return
 			if(len(node.value) > 0):
 				k[0]+=1
 			# print "Query util ", k, pos, ans, node.value
 			if(pos[0] == k[0]):
 				ans.append(node)
 				return
-			if(len(node.adjacencyListRight) > 0):
-				for i in node.adjacencyListRight:
-					j = node.adjacencyListRight[i]
-					util(j, k, pos, ans)
-					if(len(ans) > 0):
-						return
+			if(node.right != None):
+				j = node.right
+				util(j, k, pos, ans)
+				if(len(ans) > 0):
+					return
 		pos = [n]
 		k = [0]
 		ans = []
@@ -57,7 +65,6 @@ class Node:
 					
 	def conccurentInsert(self, queries):
 		d = dict()
-		dfs(self)
 
 		# print self.size, queries
 		for i in queries:
@@ -79,97 +86,142 @@ class Node:
 				atom, pos, S = j
 				l.append((S, pos, atom))
 			l.sort()
-			print l,i 
-			print prevSize,positions[i][0].value
+			# print l,i 
+			# print prevSize,positions[i][0].value
+			# if(len(positions[i])>1):
+ 		# 		print positions[i][1].value
 			cnt = 0;
 			if(i == 1):
 				for j in l:
 					S, pos, atom = j
-					positions[i][0].adjacencyListRight[(1, (S, positions[i][0].counter+1))] = Node(atom)
+					positions[i][0].right = Node(atom)
+					positions[i][0].right.parent = positions[i][0]
+					positions[i][0].size+=1
+					par = positions[i][0].parent
+					while( par != None ):
+						par.size+=1
+						par = par.parent
+					positions[i][0] = positions[i][0].right
 				positions[i][0].counter+=1	
 			elif(i == prevSize):
 				for j in l:
 					S, pos, atom = j
-					positions[i][0].adjacencyListRight[(1,(S,positions[i][0].counter+1))] = Node(atom)
+					positions[i][0].right = Node(atom)
+					positions[i][0].right.parent = positions[i][0]
+					positions[i][0].size+=1
+					par = positions[i][0].parent
+					while( par != None ):
+						par.size+=1
+						par = par.parent
+					positions[i][0] = positions[i][0].right
 				positions[i][0].counter+=1	
 			else:
-				b = copy(positions[i][0])
-				f = copy(positions[i][1])
-				if(f in b.adjacencyListRight.values()):
-					for j in l:
+				b = positions[i][0]
+				f = positions[i][1]
+				if(ancestor(b, f)):
+					S, pos, atom = l[0]
+					f.left = Node(atom)
+					f.left.parent = f
+					f.size+=1
+					par = f.parent
+					while( par != None ):
+						par.size+=1
+						par = par.parent
+					f = f.left
+					for j in l[1:]:
 						S, pos, atom = j
-						f.adjacencyListLeft[(0,(S, f.counter+1))] = Node(atom)
+						f.right = Node(atom)
+						f.right.parent = f
+						f.size+=1
+						par = f.parent
+						while( par != None ):
+							par.size+=1
+							par = par.parent
+						f = f.right
 					f.counter+=1
-				elif(b in f.adjacencyListLeft.values()):
+				elif(ancestor(f, b)):
 					for j in l:
 						S, pos, atom = j
-						b.adjacencyListRight[(1, (S, b.counter+1))] = Node(atom)
+						b.right = Node(atom)
+						b.right.parent = b
+						b.size+=1
+						par = b.parent
+						while( par != None ):
+							par.size+=1
+							par = par.parent
+						b = b.right
 					b.counter+=1
 				else:
 					for j in l:
 						S, pos, atom = j
-						b.adjacencyListRight[(1, (S, b.counter+1))] = Node(atom)
+						b.right = Node(atom)
+						b.right.parent = b
+						b.size+=1
+						par = b.parent
+						while( par != None ):
+							par.size+=1
+							par = par.parent
+						b = b.right
 					b.counter+=1					
-								
-	 	# 	for j in l:
-			# 	S, pos, atom = j
-			# 	if(pos == 1):
-			# 		positions[j]
-			# 	positions.append(self.query(pos))
-			# 	# for k in range(self.size):
-			# 	# 	print k+1, self.query(k+1).value
-			# 	# print "endl"	
-			# 	# print j, self.query(pos).value, self.size
-			# for j in positions:
-			# 	print j.value	
-			# # for j in l:
-			# # 	S, pos, atom = j
-			# # 	pos+=cnt
-			# # 	# print j, pos
-			# # 	self.insert(atom, pos, S)
-			# # 	cnt+=1
-			# print "endfn"			
-		 
 	def insert(self, atom, insertPos, siteId):
 		if(atom == ""):
 			return -1
-		dfs(self)
 		# print "insert ", atom, insertPos, siteId,self.size
+		child = None
 		if(insertPos == 1):
 			f = self.query(insertPos)
-			f.adjacencyListLeft[(0,(siteId, f.counter+1))] = Node(atom)
+			f.left = Node(atom)
+			child = f.left
+			child.parent = f
 			f.counter += 1
 		elif(insertPos >= self.size):
 			f = self.query(self.size)		
-			f.adjacencyListRight[(1,(siteId, f.counter+1))] = Node(atom)
+			f.right = Node(atom)
+			child = f.right
+			child.parent = f
 			f.counter += 1
 		else:
 			b = None
 			b = self.query(insertPos-1)
 			f = self.query(insertPos)
-			if(f in b.adjacencyListRight.values()):
-				f.adjacencyListLeft[(0,(siteId, f.counter+1))] = Node(atom)
+			if(ancestor(b,f)):
+				f.left = Node(atom)
+				child = f.left
+				child.parent = f
 				f.counter+=1
-			elif(b in f.adjacencyListLeft.values()):
-				b.adjacencyListRight[(1, (siteId, b.counter+1))] = Node(atom)
+			elif(ancestor(f,b)):
+				b.right = Node(atom)
+				child = b.right
+				child.parent = b
 				b.counter+=1
 			else:
-				b.adjacencyListRight[(1, (siteId, b.counter+1))] = Node(atom)
-				b.counter+=1					
+				b.right = Node(atom)
+				child = b.right
+				child.parent = b
+				b.counter+=1
+		# print "child ", child.value		
+		par = child.parent
+		while( par != None):
+			par.size+=1
+			par = par.parent
+		
 	def delete(self, pos, siteId):
 		node = self.query(pos)
 		node.value = ""
-		node.size = 0
+		node.size -= 1
+		par = node.parent
+		while( par != None):
+			par.size-=1
+			par = par.parent
 
 	def flatten(self):
 		ans = [""]
-		# dfs(self)
 		def util(node, ans):
-			for i in node.adjacencyListLeft:
-				util(node.adjacencyListLeft[i], ans)
+			if(node.left != None):
+				util(node.left, ans)
 			ans[0]+=node.value
-			for i in node.adjacencyListRight:
-				util(node.adjacencyListRight[i], ans)
+			if(node.right != None):
+				util(node.right, ans)
 		util(self, ans)
 		return ans[0]
 
@@ -181,37 +233,38 @@ class Node:
 				return Node("")
 			else:
 				node = Node("")
-				node.adjacencyListLeft[(0,(0,0))] = allocate(h-1)	
-				node.adjacencyListLeft[(1,(0,0))] = allocate(h-1)
+				node.left = allocate(h-1)	
+				node.right = allocate(h-1)
 				return node
 		def fill(node, atomsting, ptr):
 			if(ptr == len(atomstring)):
 				return
 			else:
-				if(len(node.adjacencyListLeft) > 0):
-					for i in node.adjacencyListLeft:
-						j = node.adjacencyListLeft[i]
-						fill(j, atomstring, ptr)
+				if(node.left != None):
+					fill(node.left, atomstring, ptr)
 				node.value = atomstring[ptr[0]]
 				ptr[0]+=1
-				if(len(node.adjacencyListRight) > 0):
-					for i in node.adjacencyListRight:
-						j = node.adjacencyListRight[i]
-						fill(j, atomstring, ptr)
+				if(node.right != None):
+					fill(node.right, atomstring, ptr)
 		allocate(h)
 		ptr = [0]
 		fill(self, atomstring, ptr)
 
 def main():
-	crdt = Node("_")
+	crdt = Node("")
 	crdt.insert("hi",1,1)
 	crdt.insert("!",2,2)
 	crdt.insert("how are you",3,1)
-	crdt.delete(3,2)
-	conccurentQueries = [["this assignment ", 3, 1],["was fun ", 3, 2],[" this is khalid, ",2,1], [" this is khalid, ",2,2]]
+	crdt.insert("how are you ",3,2)
+	crdt.delete(4,2)
+	conccurentQueries = [["this assignment ", 3, 1],["was fun\n", 3, 2],[" this is khalid, ",2,1], [" this is khalid, ",2,2]]
 	crdt.conccurentInsert(conccurentQueries)
 	crdt.delete(3,2)
 	crdt.delete(3,2)
+	crdt.insert("\nBut not as easy\n", crdt.size,1)
+	crdt.insert(", ",4, 1)
+	crdt.insert("Since It has been a whlie\n",crdt.size, 2)
+	crdt.insert("Since I coded in python\n",crdt.size, 1)
 	s = crdt.flatten()
 	print s
 main()
