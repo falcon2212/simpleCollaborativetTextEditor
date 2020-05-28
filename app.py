41cfffa2142ec74903c6c408f350428494ae3a0a
falcon2212 <khalid.mrkool@gmail.com>
2020-05-29 01:40:38 +0530

    resolved the issue of updating query number

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import psycopg2
 from treedoc import *
 from persist import persist
+
 class CRDT():
 	def __init__(self):
 		self.treedoc = Node("")
@@ -13,10 +14,19 @@ class CRDT():
 		        cmd = "CREATE TABLE IF NOT EXISTS delete_query_db ( id integer PRIMARY KEY,timestamp TIMESTAMP NOT NULL,"
 		        cmd+="type VARCHAR(10), pos integer, siteid integer);"
 		        cur.execute(cmd)
+		        cmd = "SELECT MAX(id) FROM delete_query_db"
+		        cur.execute(cmd)
+		        self.queryNumber = max(self.queryNumber, cur.fetchone()[0])
 		        cmd = "CREATE TABLE IF NOT EXISTS insert_query_db ( id integer PRIMARY KEY,timestamp TIMESTAMP NOT NULL,"
 		        cmd+="type VARCHAR(10), atom  VARCHAR(255), pos integer, siteid integer);"
 		        cur.execute(cmd)
-
+		        cmd = "SELECT MAX(id) FROM insert_query_db"
+		        cur.execute(cmd)
+		        self.queryNumber = max(self.queryNumber, cur.fetchone()[0])
+		self.latestQueryNumber = self.queryNumber
+		self.queryNumber+=1
+		data = retrieve()
+		self.treedoc = reconstruct(data[0], data[1])
 	def insert(self, atom, pos, siteid, timestamp):
 		self.queryNumber+=1
 		self.queryList.append([timestamp, "insert", [atom, pos, siteid]])
@@ -66,32 +76,31 @@ class CRDT():
 			q = [self.queryDict[i]["insert"],self.queryDict[i]["delete"]]
 			self.treedoc.conccurentOperations(q[0], q[1])	
 		self.latestQueryNumber = self.queryNumber
-		return self.treedoc.flatten()		
+		return self.treedoc.flatten()
 	def saveDocument(self):
+		self.getdocument()
 		data = getDataFromCRDT(self.treedoc)
 		persist(data)
-		del self.treedoc	
 	def retrieveDocument(self):
 		data = retrieve()
-		self.treedoc = reconstruct(data[0],data[1])		
+		self.treedoc = reconstruct(data[0],data[1])
+		dfs(self.treedoc)		
 if __name__ == "__main__":
-        crdt = CRDT()	
-        crdt.insert("a", 1, 1, '2011-05-16 15:36:38')		
-        crdt.insert("b", 1, 2, '2011-05-16 15:36:38')
-        crdt.insert("c", 1, 1, '2011-05-16 15:37:38')		
-        print crdt.getdocument()
-        crdt.insert("d", 1, 2, '2011-05-16 15:37:38')		
-        crdt.delete( 1, 2, '2011-05-16 15:37:38')	
-        print crdt.getdocument()	
-        crdt.insert("e", 1, 2, '2011-05-16 15:37:38')		
-        crdt.delete( 1, 2, '2011-05-16 15:37:38')		
-        print crdt.getdocument()	
-        crdt.insert("f", 1, 2, '2011-05-16 15:37:38')		
-        crdt.insert("g", 1, 2, '2011-05-16 15:37:38')		
-        crdt.insert("h", 1, 2, '2011-05-16 15:37:38')		
-        crdt.delete( 3, 2, '2011-05-16 15:39:38')		
-        print crdt.getdocument()
-        crdt.saveDocument()
-        crdt.retrieveDocument()
-        print crdt.getdocument()
-        
+	crdt = CRDT()	
+	crdt.insert("a", 1, 1, '2011-05-16 15:36:38')		
+	crdt.insert("b", 1, 2, '2011-05-16 15:36:38')
+	crdt.insert("c", 1, 1, '2011-05-16 15:37:38')		
+	crdt.insert("d", 1, 2, '2011-05-16 15:37:38')		
+	crdt.insert("d", 2, 1, '2011-05-16 15:37:38')		
+	crdt.insert("d", 2, 2, '2011-05-16 15:37:38')		
+	crdt.insert("e", 1, 1, '2011-05-16 15:37:38')	
+	crdt.saveDocument()
+	crdt.retrieveDocument()
+	crdt.delete( 1, 2, '2011-05-16 15:37:39')	
+	crdt.insert("f", 3, 2, '2011-05-16 15:37:41')		
+	crdt.insert("g", 1, 2, '2011-05-16 15:37:42')		
+	crdt.delete( 1, 2, '2011-05-16 15:37:40')		
+	crdt.saveDocument()
+	crdt.retrieveDocument()
+	crdt.insert("e", 5, 2, '2011-05-16 15:37:39')		
+	print crdt.getdocument()
